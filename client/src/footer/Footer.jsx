4388621a2567d0e3715cd34937903ec3d9c20d85
Relanaria <shadowword9@gmail.com>
2024-07-25 17:40:35 +0300

    Update href

--- client/src/footer/Footer.jsx
+++ client/src/footer/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
                     <div className="footer-section links">
                         <h2>Quick Links</h2>
                         <ul>
-                            <li><a href="/home">Home</a></li>
+                            <li><a href="/">Home</a></li>
                             <li><a href="/catalog">Catalog</a></li>
                             <li><a href="/store">Store</a></li>
                             <li><a href="/about">About</a></li>
